a43e06b04bff9252886b2b5081e10350a8f70a24
Emilly Paiva <emillypaiva3260@gmail.com>
2026-04-27 16:33:42 -0300

    :sparkles: feat: integrate Lenis for buttery-smooth scroll experience

---
+++ src/hooks/useSmoothScroll.js
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+import Lenis from 'lenis'
+
+export function useSmoothScroll() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.25,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      lerp: 0.1
+    })
+    let raf
+    function loop(time) {
+      lenis.raf(time)
+      raf = requestAnimationFrame(loop)
+    }
+    raf = requestAnimationFrame(loop)
+    return () => {
+      cancelAnimationFrame(raf)
+      lenis.destroy()
+    }
+  }, [])
+}
